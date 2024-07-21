d3668bb6387700b7857421de2591ab9832886dce
Eractrus de Judá <eractrus@gmail.com>
2024-07-21 15:13:01 -0300

    create project

--- fullstack/devInvest/backend/src/routes/dataWebRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const dataActiveRoute = require('../controller/getDataActiveController');
-router.get('/', new dataActiveRoute().handle);
-
-
-module.exports = router;
